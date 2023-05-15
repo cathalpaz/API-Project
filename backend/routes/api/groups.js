a0587d3f9f01f1a3f33d78b4cfd0c038367bb9ce
Cathal <cathalpaz@gmail.com>
2023-05-15 18:09:32 -0400

    route updates

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const { Group } = require('../../db/models');
+const { Group, Membership, GroupImage } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth')
 
+
+// Get all Groups
 router.get('/', async (req, res) => {
     const groups = await Group.findAll()
-
-    return res.json(groups)
+    for (let group of groups) {
+        let numMembers = await Membership.findAll({
+            where: {
+                groupId: group.id
+            }
+        })
+        group.dataValues.numMembers = numMembers.length
+    }
+    return res.json({Groups: groups})
 })
+
+// Get all Groups joined or organized by the Current User
+router.get('/current', requireAuth, async(req, res) => {
+
+})
+
+
+
+
 
 module.exports = router;
