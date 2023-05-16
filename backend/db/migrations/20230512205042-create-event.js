98ac5e929e4b12f7e2733167a448cd4f44685bc0
Cathal <cathalpaz@gmail.com>
2023-05-16 18:46:57 -0400

    capitalization error

--- backend/db/migrations/20230512205042-create-event.js
+++ backend/db/migrations/20230512205042-create-event.js
@@ -37,7 +37,7 @@ module.exports = {
         allowNull: false
       },
       type: {
-        type: Sequelize.ENUM('Online', 'In Person')
+        type: Sequelize.ENUM('Online', 'In person')
       },
       capacity: {
         type: Sequelize.INTEGER,
