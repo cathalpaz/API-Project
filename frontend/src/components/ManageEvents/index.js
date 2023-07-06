4ef1e1f099870fef74dc3de3f4b0ccae4a76e128
Cathal <cathalpaz@gmail.com>
2023-07-05 23:04:51 -0400

    manage group + events

--- frontend/src/components/ManageEvents/index.js
+++ frontend/src/components/ManageEvents/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function ManageEvents() {
   return (
-    <div>ManageEvents</div>
+    <div className="no-groups">
+          <img src="https://secure.meetupstatic.com/next/images/home/EmptyGroup.svg?w=256"/>
+          <h2>No events attending</h2>
+          <span>All events you can attend and organize will be shown here.</span>
+        </div>
   )
 }
 
--- frontend/src/components/ManageGroups/index.js
+++ frontend/src/components/ManageGroups/index.js
@@ -24,53 +24,60 @@ function ManageGroups() {
         <i class="fa-solid fa-arrow-left"></i>
         <NavLink to="/">Back to home page</NavLink>
       </div>
-      <div className="list-header-links">Manage Groups</div>
-      <div className="list-display">
-        <h3>Your groups in .join(Up)</h3>
-        <div className="list-item">
-          {myGroups.map((group) => {
-            return (
-              <div>
-                <NavLink
-                  key={group.id}
-                  to={`/groups/${group.id}`}
-                  className="display-container"
-                >
-                  <div className="left-container">
-                    <img alt="group-pic" src={group.previewImage}></img>
-                  </div>
-                  <div className="right-container">
-                    <div className="right-upper-container">
-                      <h2>{group.name}</h2>
-                      <h4>
-                        {group.city}, {group.state}
-                      </h4>
+      {myGroups.length > 0 ? (
+        <div className="list-display">
+          <div className="list-header-links">Manage Groups</div>
+          <h3>Your groups in .join(Up)</h3>
+          <div className="list-item">
+            {myGroups.map((group) => {
+              return (
+                <div>
+                  <NavLink
+                    key={group.id}
+                    to={`/groups/${group.id}`}
+                    className="display-container"
+                  >
+                    <div className="left-container">
+                      <img alt="group-pic" src={group.previewImage}></img>
                     </div>
-                    <p>{group.about}</p>
-                    <div className="bottom-container">
-                      <p>{group.numMembers} members</p>
-                      <p>•</p>
-                      <p>{group.private ? "Private" : "Public"}</p>
+                    <div className="right-container">
+                      <div className="right-upper-container">
+                        <h2>{group.name}</h2>
+                        <h4>
+                          {group.city}, {group.state}
+                        </h4>
+                      </div>
+                      <p>{group.about}</p>
+                      <div className="bottom-container">
+                        <p>{group.numMembers} members</p>
+                        <p>•</p>
+                        <p>{group.private ? "Private" : "Public"}</p>
+                      </div>
+                      {group.organizerId === user.id ? (
+                          <div className="manage-btns">
+                              <button>Update</button>
+                              <OpenModalButton modalComponent={<DeleteGroup />} buttonText={'Delete'}/>
+                          </div>
+                      ): (
+                          <div className="manage-btns">
+                              <button>Leave</button>
+                          </div>
+                      )}
                     </div>
-                    {group.organizerId === user.id ? (
-                        <div className="manage-btns">
-                            <button>Update</button>
-                            <OpenModalButton modalComponent={<DeleteGroup />} buttonText={'Delete'}/>
-                        </div>
-                    ): (
-                        <div className="manage-btns">
-                            <button>Unjoin</button>
-                        </div>
-                    )}
-
-
-                  </div>
-                </NavLink>
-              </div>
-            );
-          })}
+                  </NavLink>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
+
+      ) : (
+        <div className="no-groups">
+          <img src="https://secure.meetupstatic.com/next/images/home/EmptyGroup.svg?w=256"/>
+          <h2>No groups joined</h2>
+          <span>All groups you can join and organize will be shown here.</span>
+        </div>
+      )}
     </div>
   );
 }
