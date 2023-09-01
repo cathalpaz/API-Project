438f831835c04e70b5b09dd3df8c5d8b5a67dfe0
Cathal <cathalpaz@gmail.com>
2023-08-31 23:01:03 -0400

    update links

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -15,9 +15,9 @@ function Footer() {
             <div className='footer-about_me'>
                 <p>Find out more about me</p>
                 <div className='footer-links'>
-                    <a href=''><i className="fa-solid fa-laptop"></i></a>
+                    <a href='https://cathalpaz.com'><i className="fa-solid fa-laptop"></i></a>
                     <a href='https://www.github.com/cathalpaz'><i className="fa-brands fa-github"></i></a>
-                    <a href='https://www.linkedin.com/in/cathal-paz-052239263/'><i className="fa-brands fa-linkedin"></i></a>
+                    <a href='https://www.linkedin.com/in/cathal-paz/'><i className="fa-brands fa-linkedin"></i></a>
                 </div>
             </div>
             <p className='footer-tag'>© Cathal Paz</p>
